e201cdc1f77dff28efc98842edac7864b242108e
Aaron Ross <superhawk610@gmail.com>
2026-06-11 12:08:02 -0700

    bump index version
    Parsing HEEX sigils will introduce new references.

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,4 +5,4 @@ const Version = "0.7.1"
 // IndexVersion is incremented whenever the index schema or parser changes in a
 // way that requires a full rebuild. Bump this alongside Version when releasing
 // a change that makes existing indexes stale.
-const IndexVersion = 12
+const IndexVersion = 13
